aaafc54486ed1878ef3974c5f2fdea0706cb8c56
tbriany <brianytaveras@pursuit.org>
2020-11-22 21:15:07 -0500

    creates search bar and movie page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link, withRouter, Redirect } from "react-router-dom";
+import MoviePage from "./Components/moviePage";
+import SearchBar from "./Components/searchBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>movie app</h1>
+      <SearchBar/>
     </div>
   );
 }
---
+++ frontend/src/Components/moviePage.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link, withRouter, Redirect} from "react-router-dom";
+
+function MoviePage () {
+  return (
+    <div className="moviePage">
+      <h1>movie PAGE!!</h1>      
+    </div>
+  );
+}
+
+export default MoviePage;
