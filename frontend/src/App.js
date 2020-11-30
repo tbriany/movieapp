46c6d8a47578b25bda31f89e305eccedcbea3f32
tbriany <brianytaveras@pursuit.org>
2020-11-29 23:46:57 -0500

    creates routes for search and movie page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Link, withRouter, Redirect } from "react-router-dom";
-import moviePage from "./Components/moviePage";
+import MoviePage from "./Components/moviePage";
 import SearchBar from "./Components/searchBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>movie app</h1>
-      <SearchBar/>
+      <Switch>
+      <Route exact path="/" component={SearchBar} />
+        <Route exact path="/movie/:id" component={MoviePage} />
+      </Switch>
     </div>
   );
 }
