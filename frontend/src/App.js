c80bec6099d5cdad561e9daa95b59c551cb7bd57
tbriany <brianytaveras@pursuit.org>
2020-11-29 22:19:22 -0500

    creates search bar and movie page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Link, withRouter, Redirect } from "react-router-dom";
-import MoviePage from "./Components/moviePage";
+import moviePage from "./Components/moviePage";
 import SearchBar from "./Components/searchBar";
 
 function App() {
--- frontend/src/Components/searchBar.jsx
+++ frontend/src/Components/searchBar.jsx
@@ -1,16 +1,49 @@
 import '../App.css';
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Link, withRouter, Redirect } from "react-router-dom";
-
+import axios from "axios";
 
 function SearchBar() {
+    
+    const [input, updateInput] = useState('')
+
+    const handleSearch = () => {
+        console.log('searching for movie', input)
+        const options = {
+            method: 'GET',
+            url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
+            params: {s: 'Avengers Endgame', page: '1', r: 'json'},
+            // params: {s: input, page: '1', r: 'json'},
+            headers: {
+              'x-rapidapi-key': process.env.REACT_APP_MOVIE_API_KEY,
+              'x-rapidapi-host': process.env.REACT_APP_MOVIE_API_HOST
+            }
+          };
+          
+          axios.request(options).then(function (response) {
+              console.log(response.data);
+          }).catch(function (error) {
+              console.error(error);
+          });
+    }
+
+    const handleInput = (event) => {
+        let search = event.target.value
+        updateInput(search)
+        console.log('after',input)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        handleSearch()
+    }
 
     return (
         <div className="searchBar">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
-                    Movie title: 
-                    <input type="text" name="title" />
+                    Movie title:
+                    <input type="text" name="title" onChange={handleInput}/>
                 </label>
                 <input type="submit" value="Submit" />
             </form>
