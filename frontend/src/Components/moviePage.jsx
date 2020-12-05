da0c840a442d1d5dfff5c9b6d7559dcb9bb29e3f
tbriany <brianytaveras@pursuit.org>
2020-12-04 22:02:42 -0500

    displays movie info, updates likes and dislikes

--- frontend/src/Components/moviePage.jsx
+++ frontend/src/Components/moviePage.jsx
@@ -1,51 +1,96 @@
 import '../App.css';
 import React, { useState, useEffect } from "react";
-import { Switch, Route, Link, withRouter, Redirect } from "react-router-dom";
+import {Link} from "react-router-dom";
 import axios from "axios";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
 
 function MoviePage(props) {
-    console.log(props.match.params.id)
     const [movie, setMovie] = useState({})
+    const [likes, setLikes] = useState(0)
+    const [dislikes, setDislikes] = useState(0)
+
+
+    const getMovieInfo = async () => {
+        try {
+            const res = await axios.get(`/movies/info/${movie.Title}`)
+            setLikes(res.data.payload[0].thumbs_up)
+            setDislikes(res.data.payload[0].thumbs_down)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     useEffect(() => {
         async function fetchMovieData() {
-          try {
-            const options = {
-                method: 'GET',
-                url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
-                params: {i: props.match.params.id, r: 'json'},
-                headers: {
-                    'x-rapidapi-key': process.env.REACT_APP_MOVIE_API_KEY,
-                    'x-rapidapi-host': process.env.REACT_APP_MOVIE_API_HOST
-                }
-              };
-              
-              axios.request(options).then(function (response) {
-                  console.log(response.data);
-                  setMovie(response.data);
-              }).catch(function (error) {
-                  console.error(error);
-              });
-          } catch (error) {
-            setMovie({})
-            console.log(error);
-          }
+            try {
+                const options = {
+                    method: 'GET',
+                    url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
+                    params: { i: props.match.params.id, r: 'json' },
+                    headers: {
+                        'x-rapidapi-key': process.env.REACT_APP_MOVIE_API_KEY,
+                        'x-rapidapi-host': process.env.REACT_APP_MOVIE_API_HOST
+                    }
+                };
+
+                axios.request(options).then(function (response) {
+                    setMovie(response.data);
+                }).catch(function (error) {
+                    console.error(error);
+                });
+            } catch (error) {
+                setMovie({})
+                console.log(error);
+            }
         }
         fetchMovieData()
-      }, [])
+        getMovieInfo()
+    }, [props.match.params.id])
 
-    return (
-        <div className="moviePage">
+    const addLike = async (e) => {
+        try {
+            await axios.post(`/movies/updateLikes/${movie.Title}`)
+        } catch (err) {
+            console.log(err)
+        }
+        setLikes(prev => prev + 1)
+        getMovieInfo()
+    }
+
+    const addDislike = async (e) => {
+        try {
+            await axios.post(`/movies/updateDislikes/${movie.Title}`)
+        } catch (err) {
+            console.log(err)
+        }
+        setDislikes(prev => prev + 1)
+        getMovieInfo()
+}
+
+return (
+    <div className="moviePage">
+        <div>
+        <Link to={`/`}> Back to search </Link>
+            <h2>{movie.Title}</h2>
+            <img src={movie.Poster} alt={movie.Title}></img>
             <div>
-                <h2>{movie.Title}</h2>
-                <img src={movie.Poster} alt={movie.Title}></img>
-                <h4>Director: {movie.Director} </h4>
-                <h4>Release Year: {movie.Year}</h4>
-                <p>Plot: {movie.Plot}</p>
+                <span>
+                    {likes} likes
+                <FontAwesomeIcon icon={faThumbsUp} onClick={addLike} style={{ padding: "15px" }} />
+                </span>
+                <span>
+                    {dislikes} dislikes
+                <FontAwesomeIcon icon={faThumbsDown} onClick={addDislike} style={{ padding: "15px" }} />
+                </span>
             </div>
+            <h4>Director: {movie.Director} </h4>
+            <h4>Release Year: {movie.Year}</h4>
+            <p>Plot: {movie.Plot}</p>
         </div>
-    );
+    </div>
+);
 }
 
 export default MoviePage;
